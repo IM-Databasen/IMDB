7b313c8e67be9a0e89a9cab2453df016735faccf
Lars Erik Nordbø <lartrax909@gmail.com>
2022-10-27 22:52:49 +0200

    add predetermined sizes to images

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-gray-100 flex flex-row items-center justify-between p-5 h-fit sm:h-20">
     <div class="flex flex-col items-center text-4xl font-extralight sm:flex-row">
-      <img src="../static/faviconV2.avif" alt="IMDB Logo" class="w-16 ml-5" />
+      <img src="../static/faviconV2.avif" alt="IMDB Logo" width="256px" height="256px" class="w-16 ml-5" />
       <h1 class="pl-5 sm:pl-0">IMDB</h1>
     </div>
     <div class="flex flex-col sm:flex-row sm:ml-6">
--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,26 @@
       <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div class="md:5/12 lg:w-5/12">
-            <img src="../static/images/placeholder650_550.webp" alt="image" loading="lazy"
-              width="" height="">
+            <img 
+            src="../static/images/placeholder650_550.webp" 
+            alt="image" 
+            loading="lazy" 
+            width="650px" 
+            height="550px">
           </div>
           <div class="md:7/12 lg:w-6/12">
-            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">IMDB - Få svarene te dine prøver</h2>
-            <p class="mt-6 text-gray-600">Yes. vetkje ka eg ska stappa her.</p>
-            <p class="mt-4 text-gray-600">Hmm ja. Her kan du då altså få svar te prøvene du har på IM.</p>
-            <p class="mt-4 text-gray-600">Bildet te venstre e iallefall ikkje stjåle fra ei elle anna random nettsida</p>
+            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">
+              IMDB - Få svarene te dine prøver
+            </h2>
+            <p class="mt-6 text-gray-600">
+              Yes. vetkje ka eg ska stappa her.
+            </p>
+            <p class="mt-4 text-gray-600">
+              Hmm ja. Her kan du då altså få svar te prøvene du har på IM.
+            </p>
+            <p class="mt-4 text-gray-600">
+              Bildet te venstre e iallefall ikkje stjåle fra ei elle anna random nettsida
+            </p>
           </div>
         </div>
       </div>
@@ -38,7 +50,7 @@ export default {
     return {};
   },
   methods: {},
-  async mounted() {},
+  async mounted() { },
   computed: {},
 };
 </script>
