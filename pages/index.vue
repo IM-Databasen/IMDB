1f8409e9b1c02c71e0c9b8a1271da56772cbd975
Lars Erik Nordbø <lartrax909@gmail.com>
2022-10-29 22:49:41 +0200

    preload images test 2

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,7 @@
       <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div class="md:5/12 lg:w-5/12">
-            <link rel="preload" href="../static/images/placeholder650_550.webp" type="image/webp" />
-            <img src="../static/images/placeholder650_550.webp" alt="image" width="650px" height="550px">
+            <nuxt-img src="/placeholder650_550.webp" preload width="650px" height="550px" />
           </div>
           <div class="md:7/12 lg:w-6/12">
             <h1 class="text-2xl text-gray-900 font-bold md:text-4xl">
@@ -27,7 +26,6 @@
 
 <script>
 import Suggestions from '~/components/Suggestions.vue';
-
 export default {
     head: {
         title: "IMDB - Home",
