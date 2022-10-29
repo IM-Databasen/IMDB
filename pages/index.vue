f2420a601166e1e9d55d7c2ff65eee687ede199a
Lars Erik Nordbø <lartrax909@gmail.com>
2022-10-29 22:58:48 +0200

    fem

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div class="md:5/12 lg:w-5/12">
-            <nuxt-img src="/placeholder650_550.webp" preload alt="image placeholder" width="650px" height="550px" />
+            <nuxt-img src="/images/placeholder650_550.webp" preload alt="image placeholder" width="650px" height="550px" />
           </div>
           <div class="md:7/12 lg:w-6/12">
             <h1 class="text-2xl text-gray-900 font-bold md:text-4xl">
