c13a2bfa0f851cd49379364866fb0c6bbf60f5bb
arvidwedtstein <bassgamer03@gmail.com>
2022-10-31 07:18:05 +0100

    content figs?

--- pages/tests/index.vue
+++ pages/tests/index.vue
@@ -47,7 +47,7 @@ export default {
     }
   },
   async asyncData({ $content, $http }) {
-    const cards = await $content("tests").fetch();
+    const cards = await this.$content("tests").fetch();
 
     return { cards };
   },
