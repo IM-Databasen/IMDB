1897fb493720912b5331ee93bea8af4c31f71ce8
arvidwedtstein <bassgamer03@gmail.com>
2022-10-31 12:18:58 +0100

    removed uniused ipx file

--- pages/memes.vue
+++ pages/memes.vue
@@ -3,7 +3,7 @@
     <div
       class="w-full max-w-5xl p-5 pb-10 mx-auto mb-10 gap-5 columns-3 space-y-5"
     >
-      <nuxt-img preload v-for="meme in memes" :key="meme.pathShort" :src="meme.pathLong" :alt="meme.pathShort" />
+      <nuxt-img preload v-for="meme in memes" :key="meme.pathShort" :src="meme.pathShort" :alt="meme.pathShort" />
     </div>
   </div>
 </template>
@@ -26,7 +26,7 @@ export default {
     }
   },  
   mounted() {
-    this.importAll(require.context('../static/memes/', true, /\.(jpe?g|png|webp|gif)$/i));
+    this.importAll(require.context('../assets/memes/', true, /\.(jpe?g|png|webp|gif)$/i));
   },  
   methods: {
     importAll(r) {
