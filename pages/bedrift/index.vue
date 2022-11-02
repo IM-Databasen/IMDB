2dd7089046264be1a14a43ee6c27fe09518f859f
arvidwedtstein <bassgamer03@gmail.com>
2022-11-02 12:07:28 +0100

    bedrift layout fixed

--- pages/bedrift/index.vue
+++ pages/bedrift/index.vue
@@ -36,74 +36,97 @@
       </div>
     </form>
     <div class="grid grid-cols-2">
-      <!-- TODO: Lag bedre layout -->
       <div
-        class="blog-card flex flex-col m-5 shadow-xl bg-white leading-snug overflow-hidden rounded"
+        v-for="i in 6"
+        class="group flex flex-col m-5 shadow-xl bg-white leading-snug overflow-hidden rounded odd:lg:flex-row even:lg:flex-row-reverse lg:max-w-screen-md"
       >
-        <div class="meta relative">
+        <div class="relative h-52 lg:h-auto lg:basis-2/5 meta">
           <div
-            class="absolute top-0 bottom-0 left-0 right-0 photo"
-            style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg);"
+            class="absolute top-0 bottom-0 left-0 right-0 bg-cover bg-center transition-transform bg-[url('https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg')] group-hover:rotate-[3deg] group-hover:scale-[1.2]"
           ></div>
-          <ul class="m-auto list-style-none absolute top-0 bottom-0 -left-full w-full p-[10px] bg-black bg-opacity-60 text-white details">
-            <li class="inline-block author"><a href="#">John Doe</a></li>
-            <li class="inline-block date">Aug. 24, 2015</li>
-            <li class="inline-block tags">
-              <ul class="inline-block">
-                <li><a href="#">Learn</a></li>
-                <li><a href="#">Code</a></li>
-                <li><a href="#">HTML</a></li>
-                <li><a href="#">CSS</a></li>
+          <ul
+            class="m-auto list-style-none absolute top-0 bottom-0 -left-full w-full p-[10px] bg-black bg-opacity-60 text-white text-sm transition-[left] group-[.alt]:pl-6 group-hover:left-0"
+          >
+            <li class="inline-block before:mr-2.5"><a href="#">John Doe</a></li>
+            <li class="inline-block before:mr-2.5">Aug. 24, 2015</li>
+            <li class="inline-block">
+              <ul class="before:mr-2.5 first:-ml-1">
+                <li class="inline-block mr-0.5"><a href="#">Learn</a></li>
+                <li class="inline-block mr-0.5"><a href="#">Code</a></li>
+                <li class="inline-block mr-0.5"><a href="#">HTML</a></li>
+                <li class="inline-block mr-0.5"><a href="#">CSS</a></li>
               </ul>
             </li>
           </ul>
         </div>
-        <div class="description">
-          <h1>Learning to Code</h1>
-          <h2>Opening a door to the future</h2>
-          <p>
+        <div
+          class="relative bg-white p-4 z-10 lg:basis-3/5 lg:before:-skew-x-3 lg:before:bg-white lg:before:w-7 lg:before:absolute lg:before:-left-2.5 lg:before:z-10 group-[.alt]:before:-right-2.5 before:group-[.alt]:left-[inherit] before:group-[.alt]:skew-x-3"
+        >
+          <h1 class="leading-none m-0 text-2xl">Learning to Code</h1>
+          <h2 class="text-base font-light uppercase mt-1.5 text-[#a2a2a2]">
+            Opening a door to the future
+          </h2>
+          <p
+            class="relative mt-4 first-of-type:mt-5 before:absolute before:h-1 before:w-9 before:bg-[#5ad67d] before:-top-3 before:rounded"
+          >
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
             dolorum architecto obcaecati enim dicta praesentium, quam nobis!
             Neque ad aliquam facilis numquam. Veritatis, sit.
           </p>
-          <p class="read-more">
-            <a href="#">Read More</a>
+          <p class="relative mt-4 text-right first-of-type:mt-5 read-more">
+            <a
+              class="underline decoration-dotted text-[#5ad67d] inline-block relative after:content-['👉'] after:ml-2.5 after:align-middle after:opacity-0 after:transition-[margin,opacity] hover:after:opacity-100 hover:after:ml-1.5"
+              href="#"
+              >Read More</a
+            >
           </p>
         </div>
       </div>
-      <div
-        class="blog-card flex flex-col m-5 shadow-xl bg-white leading-snug overflow-hidden rounded alt"
+      <!-- <div
+        v-for="i in 6"
+        class="group flex flex-col m-5 shadow-xl bg-white leading-snug overflow-hidden rounded lg:max-w-screen-md lg:flex-row-reverse alt"
       >
-        <div class="relative meta">
+        <div class="relative h-52 lg:h-auto lg:basis-2/5 meta">
           <div
-            class="absolute top-0 bottom-0 left-0 right-0 photo"
-            style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg);"
+            class="absolute top-0 bottom-0 left-0 right-0 bg-cover bg-center transition-transform bg-[url('https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg')] group-hover:rotate-[3deg] group-hover:scale-[1.2]"
           ></div>
-          <ul class="m-auto list-style-none absolute top-0 bottom-0 -left-full w-full p-[10px] bg-black bg-opacity-60 text-white details">
-            <li class="inline-block author"><a href="#">Jane Doe</a></li>
-            <li class="inline-block date">July. 15, 2015</li>
-            <li class="inline-block tags">
-              <ul class="inline-block">
-                <li><a href="#">Learn</a></li>
-                <li><a href="#">Code</a></li>
-                <li><a href="#">JavaScript</a></li>
+          <ul
+            class="m-auto list-style-none absolute top-0 bottom-0 -left-full w-full p-[10px] bg-black bg-opacity-60 text-white text-sm transition-[left] group-[.alt]:pl-6 group-hover:left-0"
+          >
+            <li class="inline-block before:mr-2.5"><a href="#">Jane Doe</a></li>
+            <li class="inline-block before:mr-2.5">July. 15, 2015</li>
+            <li class="inline-block">
+              <ul class="before:mr-2.5 first:-ml-1">
+                <li class="inline-block mr-0.5"><a href="#">Learn</a></li>
+                <li class="inline-block mr-0.5"><a href="#">Code</a></li>
+                <li class="inline-block mr-0.5"><a href="#">JavaScript</a></li>
               </ul>
             </li>
           </ul>
         </div>
-        <div class="description">
-          <h1>Mastering the Language</h1>
-          <h2>Java is not the same as JavaScript</h2>
-          <p>
+        <div
+          class="relative bg-white p-4 z-10 lg:basis-3/5 lg:before:-skew-x-3 lg:before:bg-white lg:before:w-7 lg:before:absolute lg:before:-left-2.5 lg:before:z-10 before:group-[.alt]:-right-2.5 group-[.alt]:before:left-[inherit] before:group-[.alt]:skew-x-3"
+        >
+          <h1 class="leading-none m-0 text-2xl">Mastering the Language</h1>
+          <h2 class="text-base font-light uppercase mt-1.5 text-[#a2a2a2]">
+            Java is not the same as JavaScript
+          </h2>
+          <p
+            class="relative mt-4 first-of-type:mt-5 before:absolute before:h-1 before:w-9 before:bg-[#5ad67d] before:-top-3 before:rounded"
+          >
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum
             dolorum architecto obcaecati enim dicta praesentium, quam nobis!
             Neque ad aliquam facilis numquam. Veritatis, sit.
           </p>
-          <p class="read-more">
-            <a href="#">Read More</a>
+          <p class="relative mt-4 first-of-type:mt-5 text-right read-more">
+            <a
+              class="underline decoration-dotted text-[#5ad67d] inline-block relative after:content-['👉'] after:ml-2.5 after:align-middle after:opacity-0 after:transition-[margin,opacity] hover:after:opacity-100 hover:after:ml-1.5"
+              href="#"
+              >Read More</a
+            >
           </p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -138,162 +161,3 @@ export default {
   },
 };
 </script>
-<style scoped lang="scss">
-$color_white: #fff;
-$color_prime: #5ad67d;
-$color_grey: #e2e2e2;
-$color_grey_dark: #a2a2a2;
-
-.blog-card {
-  &:hover {
-    .photo {
-      transform: scale(1.3) rotate(3deg);
-    }
-  }
-  .meta {
-    position: relative;
-    height: 200px;
-  }
-  .photo {
-    background-size: cover;
-    background-position: center;
-    transition: transform 0.2s;
-  }
-
-  .details {
-    transition: left 0.2s;
-    font-size: 0.9rem;
-    a {
-      text-decoration: dotted underline;
-    }
-    ul li {
-      display: inline-block;
-    }
-    .author:before {
-      font-family: FontAwesome;
-      margin-right: 10px;
-      content: "\f007";
-    }
-
-    .date:before {
-      font-family: FontAwesome;
-      margin-right: 10px;
-      content: "\f133";
-    }
-
-    .tags {
-      ul:before {
-        font-family: FontAwesome;
-        content: "\f02b";
-        margin-right: 10px;
-      }
-      li {
-        margin-right: 2px;
-        &:first-child {
-          margin-left: -4px;
-        }
-      }
-    }
-  }
-  .description {
-    padding: 1rem;
-    background: $color_white;
-    position: relative;
-    z-index: 1;
-    h1,
-    h2 {
-      font-family: Poppins, sans-serif;
-    }
-    h1 {
-      line-height: 1;
-      margin: 0;
-      font-size: 1.7rem;
-    }
-    h2 {
-      font-size: 1rem;
-      font-weight: 300;
-      text-transform: uppercase;
-      color: $color_grey_dark;
-      margin-top: 5px;
-    }
-    .read-more {
-      text-align: right;
-      a {
-        color: $color_prime;
-        display: inline-block;
-        position: relative;
-        &:after {
-          content: "\f061";
-          font-family: FontAwesome;
-          margin-left: -10px;
-          opacity: 0;
-          vertical-align: middle;
-          transition: margin 0.3s, opacity 0.3s;
-        }
-
-        &:hover:after {
-          margin-left: 5px;
-          opacity: 1;
-        }
-      }
-    }
-  }
-  p {
-    position: relative;
-    margin: 1rem 0 0;
-    &:first-of-type {
-      margin-top: 1.25rem;
-      &:before {
-        content: "";
-        position: absolute;
-        height: 5px;
-        background: $color_prime;
-        width: 35px;
-        top: -0.75rem;
-        border-radius: 3px;
-      }
-    }
-  }
-  &:hover {
-    .details {
-      left: 0%;
-    }
-  }
-
-  @media (min-width: 640px) {
-    flex-direction: row;
-    max-width: 700px;
-    .meta {
-      flex-basis: 40%;
-      height: auto;
-    }
-    .description {
-      flex-basis: 60%;
-      &:before {
-        transform: skewX(-3deg);
-        content: "";
-        background: #fff;
-        width: 30px;
-        position: absolute;
-        left: -10px;
-        top: 0;
-        bottom: 0;
-        z-index: -1;
-      }
-    }
-    &.alt {
-      flex-direction: row-reverse;
-      .description {
-        &:before {
-          left: inherit;
-          right: -10px;
-          transform: skew(3deg);
-        }
-      }
-      .details {
-        padding-left: 25px;
-      }
-    }
-  }
-}
-</style>
