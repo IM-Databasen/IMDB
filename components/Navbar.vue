daac1bb4b0357237c198087577066fd2126457c4
arvidwedtstein <bassgamer03@gmail.com>
2022-11-08 15:24:52 +0100

    added icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -24,9 +24,23 @@
             ? 'border-gray-900 text-gray-900'
             : 'border-transparent text-gray-600 hover:text-gray-800'
         "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2"
+        class="py-2 px-3 m-1 text-sm font-medium border-t-2 inline-flex flex-row"
       >
-        Him <i class="fas fa-home"></i>
+        Him
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
       </nuxt-link>
       <nuxt-link
         to="/about"
