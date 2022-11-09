446acc87d517fd60315dab227900076bdc9d2cf9
arvidwedtstein <bassgamer03@gmail.com>
2022-11-09 09:02:20 +0100

    Removed old navbar code and Copilot comments

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -22,8 +22,6 @@
         :key="i"
         :to="link.route"
         :class="
-          // create a regex to exactly match the current route
-          // and add the active class if it matches
           new RegExp(`^${link.route}$`, 'i').test($route.path)
             ? 'border-gray-900 text-gray-900'
             : 'border-transparent text-gray-600 hover:text-gray-800'
@@ -33,103 +31,6 @@
         {{ link.name }}
         <Icon v-if="link.icon" :name="link.icon" class="w-6 h-6 ml-2" />
       </nuxt-link>
-
-      <!-- <nuxt-link
-        to="/"
-        :class="
-          String($nuxt.$route.path) == '/'
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Him
-        <Icon name="home" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/about"
-        :class="
-          String($nuxt.$route.path) == '/about'
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Ka e IMDB?
-        <Icon name="server" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/tests"
-        :class="
-          String($nuxt.$route.path).startsWith('/tests')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Prøvar
-        <Icon name="folder" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/memes"
-        :class="
-          String($nuxt.$route.path).startsWith('/memes')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Memes
-        <Icon name="image" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/chat"
-        :class="
-          String($nuxt.$route.path).startsWith('/chat')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Chat
-        <Icon name="chat" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/fordypning"
-        :class="
-          String($nuxt.$route.path).startsWith('/fordypning')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Fordypning
-        <Icon name="screen" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/bedrift"
-        :class="
-          String($nuxt.$route.path).startsWith('/bedrift')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Bedriftar
-        <Icon name="chart" class="w-6 h-6 ml-1" />
-      </nuxt-link>
-      <nuxt-link
-        to="/laererdb"
-        :class="
-          String($nuxt.$route.path).startsWith('/laererdb')
-            ? 'border-gray-900 text-gray-900'
-            : 'border-transparent text-gray-600 hover:text-gray-800'
-        "
-        class="py-2 px-3 m-1 text-sm font-medium border-t-2 flex flex-row outline-none"
-      >
-        Lærere
-        <Icon name="users-group" class="w-6 h-6 ml-1" />
-      </nuxt-link> -->
     </div>
   </nav>
 </template>
